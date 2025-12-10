599661b394acf863f99215a1dfd6d43ae4079861
Susheel singh <120171094+susheel131@users.noreply.github.com>
2025-12-10 22:41:21 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/ecommerce/PaymentService/repository/PaymentMethodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.PaymentService.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.PaymentService.entity.PaymentMethod;
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {}
